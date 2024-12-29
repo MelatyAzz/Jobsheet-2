c54185b79e35a3731af833a1b5f24adaf4679832
Melaty Az Zahrani <azzahranimelaty@gmail.com>
2024-12-30 06:48:43 +0700

    Add files via upload

---
+++ Latihan18.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan18
+ */
+public class Latihan18 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Melaty Az Zahrani");
+        System.out.println("Saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
---
+++ Tugas18.java
@@ -0,0 +1,14 @@
+public class Tugas18 {
+
+    public static void main(String[] args) {
+    System.out.println("--Data Mahasiswa Jurusan Teknologi Informasi POLINEMA--");
+        System.out.println("Nama    : Melaty Az Zahrani");
+        System.out.println("NIM     : 244107060012");
+        System.out.println("Kelas   : 1F");
+        System.out.println("No Absen: 18");
+        System.out.println("Prodi   : D4 Sistem Informasi Bisnis");
+        System.out.println("Alamat  : Jalan Lowokdoro Gang 3 No. 38");
+        System.out.println("No HP   : 088226528553");
+        System.out.println("Hobi    : Menonton Drama");
+    }
+}
